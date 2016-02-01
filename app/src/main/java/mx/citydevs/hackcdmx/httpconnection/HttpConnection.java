c67984b0e453dfa73fcbca3db7ee5da90c9b53d9
mikesaurio <mikesaurio@gmail.com>
2016-02-01 14:40:52 -0600

    se cambia metodo get por estar deprecada

--- app/src/main/java/mx/citydevs/hackcdmx/httpconnection/HttpConnection.java
+++ app/src/main/java/mx/citydevs/hackcdmx/httpconnection/HttpConnection.java
@@ -1,19 +1,20 @@
 package mx.citydevs.hackcdmx.httpconnection;
 
+import android.os.StrictMode;
 import android.util.Log;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.nio.charset.Charset;
 
 import mx.citydevs.hackcdmx.dialogues.Dialogues;
 
@@ -26,7 +27,7 @@ public class HttpConnection {
 
     public static final String RANK = "cops/new?identification=%s&infraccion=%s&articulo=%s&coincidio=%s&documents=%s&copy=%s&latitude=19.4394829&longitude=-99.1823385&cop_id=830625";
 
-	public static String GET(String url) {
+	/*public static String GET(String url) {
 		HttpClient client = new DefaultHttpClient();
 		HttpGet request = new HttpGet(url);
 
@@ -51,9 +52,40 @@ public class HttpConnection {
 		}
 		
 		return result;
-	}
+	}*/
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static String GET(String url) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        String string_json= null;
+        InputStream is= null;
+        try {
+            is = new URL(url).openStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            string_json =  readAll(rd);
+        } catch (Exception ex){
+                Log.e(TAG_CLASS, ex.getMessage());
+				ex.printStackTrace();
+        }finally {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+        return string_json;
+    }
 	
-	public static String POST(String url, String json) {
+	/*public static String POST(String url, String json) {
 		HttpClient httpClient = new DefaultHttpClient();
 		HttpPost httpPost = new HttpPost(url);
 		
@@ -97,5 +129,5 @@ public class HttpConnection {
 			Log.e(TAG_CLASS, "Failed to create StringEntity", e);
 		}
 		return se;
-	}
+	}*/
 }
